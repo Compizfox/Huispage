4dfdd3c888c97bc5c5f6e7fdf90dbc8bdefe1ec5
Lars Veldscholte <lars@tuxplace.nl>
2023-04-19 21:29:24 +0200

    Fix indentation

--- frontend/src/stores/expenseCategories.ts
+++ frontend/src/stores/expenseCategories.ts
@@ -1,21 +1,21 @@
-import {defineStore} from 'pinia';
-import {useAuthStore} from 'stores/auth';
+import {defineStore} from 'pinia'
+import {useAuthStore} from 'stores/auth'
 
 export const useExpenseCategoriesStore = defineStore('expenseCategories', {
-  state: () => ({
+	state: () => ({
 		expenseCategories: [],
-  }),
+	}),
 
-  getters: {
-    getExpenseCategories(state) {
-      return state.expenseCategories;
-    }
-  },
+	getters: {
+		getExpenseCategories(state) {
+			return state.expenseCategories
+		}
+	},
 
-  actions: {
+	actions: {
 		async fetch() {
 			const response = await useAuthStore().request({url: 'expense_categories/', method: 'get'})
 			this.expenseCategories = response?.data
 		}
-  }
-});
+	}
+})
