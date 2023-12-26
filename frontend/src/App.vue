e64e15ffb9a9418c2ce5a5072f369ec1a02fc325
Lars Veldscholte <lars@tuxplace.nl>
2023-12-26 17:37:04 +0100

    Fix i18n locale switching

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,5 +3,13 @@
 </template>
 
 <script setup lang="ts">
+import {useAuthStore} from 'stores/auth'
+import {useI18n} from 'vue-i18n'
 
+const authStore = useAuthStore()
+const {locale} = useI18n()
+
+if (authStore.inhabitant) {
+	locale.value = authStore.inhabitant.language
+}
 </script>
--- frontend/src/boot/i18n.ts
+++ frontend/src/boot/i18n.ts
@@ -2,14 +2,10 @@ import {createI18n} from 'vue-i18n'
 import messages from 'src/i18n'
 import {boot} from 'quasar/wrappers'
 
-import {useAuthStore} from 'stores/auth'
-
-const authStore = useAuthStore()
-
 export default boot(({app}) => {
 	const i18n = createI18n({
 		legacy: false,
-		locale: authStore.inhabitant?.language,
+		locale: 'en',
 		fallbackLocale: 'en',
 		globalInjection: true,
 		messages
