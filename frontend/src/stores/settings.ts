1da289b27da74eae66a1dfe49464a9a1948cf5c5
Lars Veldscholte <lars@tuxplace.nl>
2024-02-18 20:12:44 +0100

    Add settings store

--- frontend/src/stores/settings.ts
+++ frontend/src/stores/settings.ts
@@ -1,8 +1,21 @@
 import {defineStore} from 'pinia'
+import {useAuthStore} from 'stores/auth'
+
+interface ServerSettings {
+	publicly_editable_debitors: boolean,
+}
 
 export const useSettingsStore = defineStore('settings', {
 	state: () => ({
+		server: {} as ServerSettings,
 		showAllInhabitants: false as boolean,
 		adminMode: false as boolean,
 	}),
+
+	actions: {
+		async fetch() {
+			const response = await useAuthStore().request({url: 'settings/', method: 'get'})
+			this.server = response?.data
+		}
+	}
 });
