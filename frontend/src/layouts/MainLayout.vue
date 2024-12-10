4b56d930e9d9b1acb54d310d4a67748bc2215691
Lars Veldscholte <lars@tuxplace.nl>
2024-12-10 13:46:09 +0100

    Frontend: Improve toolbar on mobile

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -6,19 +6,37 @@
 					<q-avatar>
 						<img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
 					</q-avatar>
-					Huispage
+					<span class="gt-xs inline">Huispage</span>
 				</q-toolbar-title>
 
 				<q-tabs
 					inline-label
+					:dense="$q.screen.xs"
 					outside-arrows
 				>
-					<q-route-tab :to="{ name: 'mealPlanning' }" :label="t('tabs.mealPlanning')" icon="restaurant"/>
-					<q-route-tab :to="{ name: 'expenses' }" :label="t('tabs.expenses')" icon="receipt_long"/>
-					<q-route-tab :to="{ name: 'fridgeAccounting' }" :label="t('tabs.fridgeAccounting')" icon="sports_bar"/>
-					<q-route-tab :to="{ name: 'stats' }" :label="t('tabs.stats')" icon="insights"/>
 					<q-route-tab
-						:to="{ name: 'settings' }" :label="t('tabs.settings')"
+						:to="{ name: 'mealPlanning' }"
+						:label="$q.screen.gt.xs ? t('tabs.mealPlanning'): undefined"
+						icon="restaurant"
+					/>
+					<q-route-tab
+						:to="{ name: 'expenses' }"
+						:label="$q.screen.gt.xs ? t('tabs.expenses') : undefined"
+						icon="receipt_long"
+					/>
+					<q-route-tab
+						:to="{ name: 'fridgeAccounting' }"
+						:label="$q.screen.gt.xs ? t('tabs.fridgeAccounting') : undefined"
+						icon="sports_bar"
+					/>
+					<q-route-tab
+						:to="{ name: 'stats' }"
+						:label="$q.screen.gt.xs ? t('tabs.stats') : undefined"
+						icon="insights"
+					/>
+					<q-route-tab
+						:to="{ name: 'settings' }"
+						:label="$q.screen.gt.xs ? t('tabs.settings'): undefined"
 						icon="settings"
 						v-if="authStore.inhabitant?.is_superuser"
 					/>
@@ -28,7 +46,7 @@
 
 				<q-btn-dropdown
 					flat
-					:label="$q.screen.gt.sm ? authStore.inhabitant?.nickname: ''"
+					:label="$q.screen.gt.sm ? authStore.inhabitant?.nickname: undefined"
 					icon="account_circle"
 					dense
 					padding="none"
@@ -51,7 +69,17 @@
 	</q-layout>
 </template>
 
-<style>
+<script setup lang="ts">
+import {useQuasar} from 'quasar'
+import {useI18n} from 'vue-i18n'
+import {useAuthStore} from 'stores/auth'
+
+const {t} = useI18n()
+const $q = useQuasar()
+const authStore = useAuthStore()
+</script>
+
+<style lang="scss">
 .q-page-container {
 	margin-left: auto;
 	margin-right: auto;
@@ -61,18 +89,14 @@
 .q-page {
 	padding-top: 2rem;
 }
-</style>
 
-<script setup lang="ts">
-import {useAuthStore} from 'stores/auth'
-import {useI18n} from 'vue-i18n'
-
-const {t} = useI18n()
-const authStore = useAuthStore()
-</script>
-
-<style>
 .q-tabs {
 	max-width: 70%;
+}
+
+.q-tab {
+	@media (max-width: $breakpoint-xs-max) {
+		padding: 0 2px;
+	}
 }
 </style>
