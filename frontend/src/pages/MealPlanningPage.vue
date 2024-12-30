a359446272349493276c150c2d64b50b980455c6
Lars Veldscholte <lars@tuxplace.nl>
2024-12-30 16:44:09 +0100

    MealPlanningPage: fix ISO week year handling

--- frontend/src/pages/MealPlanningPage.vue
+++ frontend/src/pages/MealPlanningPage.vue
@@ -189,11 +189,31 @@ inhabitantsStore.fetch().then(() => {
 })
 fetch()
 
+// Returns the ISO week of the date.
+function getWeek(d: Date) {
+	var date = new Date(d.getTime());
+	date.setHours(0, 0, 0, 0);
+	// Thursday in current week decides the year.
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	// January 4 is always in week 1.
+	var week1 = new Date(date.getFullYear(), 0, 4);
+	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
+	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+		- 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+// Returns the four-digit year corresponding to the ISO week of the date.
+function getWeekYear(d: Date) {
+	var date = new Date(d.getTime());
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	return date.getFullYear();
+}
+
 async function fetch() {
 	loading.value = true
 
 	const response = await authStore.request({
-		url: 'dailies/' + date.formatDate(currentDate, 'YYYY') + '/' + date.getWeekOfYear(currentDate) + '/',
+		url: 'dailies/' + getWeekYear(currentDate) + '/' + getWeek(currentDate) + '/',
 		method: 'get',
 	})
 
@@ -266,7 +286,7 @@ function getNumGuests(n: number) {
 }
 
 function getCurrentDateString(): string {
-	return date.formatDate(currentDate, 'YYYY') + ' ' + t('week') + ' ' + date.formatDate(currentDate, 'w')
+	return getWeekYear(currentDate) + ' ' + t('week') + ' ' + getWeek(currentDate)
 }
 
 function isToday(dateString: string): boolean {
