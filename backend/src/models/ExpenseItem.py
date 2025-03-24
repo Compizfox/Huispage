7a83013b90ca62d56c1dd0d187b518c10d36ff81
Lars Veldscholte <lars@tuxplace.nl>
2025-03-24 23:37:35 +0100

    Backend: add ExpenseItem (related) serializers

--- backend/src/models/ExpenseItem.py
+++ backend/src/models/ExpenseItem.py
@@ -6,4 +6,4 @@ from .Expense import Expense
 class ExpenseItem(models.Model):
 	name = models.CharField(max_length=25)
 	cost = models.DecimalField(max_digits=5, decimal_places=2)
-	expense = models.ForeignKey(Expense, on_delete=models.CASCADE)
+	expense = models.ForeignKey(Expense, on_delete=models.CASCADE, related_name='items')
