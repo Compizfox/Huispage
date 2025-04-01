52e7513f1536c2d77797573c3f687b69e00d8f22
Lars Veldscholte <lars@tuxplace.nl>
2025-04-01 15:08:07 +0200

    Birthdays: always show next 5 birthdays, use QMarkupTable

--- frontend/src/components/Birthdays.vue
+++ frontend/src/components/Birthdays.vue
@@ -7,27 +7,28 @@
 			</div>
 		</q-card-section>
 		<q-card-section>
-			<q-list dense class="rounded-borders">
-				<template
-					v-for="inhabitant in getUpcomingBirthdays()"
-					:key="inhabitant.id"
-				>
-					<q-item
-						v-if="inhabitant.date_of_birth"
+			<q-markup-table dense flat bordered>
+				<tbody>
+					<template
+						v-for="inhabitant in getUpcomingBirthdays()"
+						:key="inhabitant.id"
 					>
-						<q-item-section>
-							{{ inhabitant.nickname }}
-						</q-item-section>
-						<q-item-section>
-							{{ date.formatDate(inhabitant.date_of_birth, 'D MMMM')}}
-						</q-item-section>
-						<q-item-section>
-							{{ date.getDateDiff(new Date(), inhabitant.date_of_birth, 'years') }}
-						</q-item-section>
-					</q-item>
-				</template>
-
-			</q-list>
+						<tr v-if="inhabitant.date_of_birth">
+							<td>{{ date.formatDate(inhabitant.date_of_birth, 'D MMMM') }}</td>
+							<td>{{ inhabitant.nickname }}</td>
+							<td>{{ date.getDateDiff(new Date(), inhabitant.date_of_birth, 'years') }}</td>
+							<td>
+								<q-badge
+									v-if="date.getDateDiff(inhabitant.nextBirthday, new Date(), 'days') < 7"
+								>
+									{{ date.getDateDiff(inhabitant.nextBirthday, new Date(), 'days') }}
+									{{ t('days') }}
+								</q-badge>
+							</td>
+						</tr>
+					</template>
+				</tbody>
+			</q-markup-table>
 		</q-card-section>
 	</q-card>
 </template>
@@ -41,30 +42,29 @@ const inhabitantsStore = useInhabitantsStore()
 const {t} = useI18n()
 
 function getNextBirthday(dob: string) {
-	const today = new Date();
-	const birthDate = new Date(dob);
+	const today = new Date()
+	const birthDate = new Date(dob)
 
 	// Set the birthday to this year
-	let nextBirthday = new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+	let nextBirthday = new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate())
 
 	// If the birthday has already passed this year, set it to next year
 	if (nextBirthday < today) {
-		nextBirthday.setFullYear(today.getFullYear() + 1);
+		nextBirthday.setFullYear(today.getFullYear() + 1)
 	}
 
-	return nextBirthday;
+	return nextBirthday
 }
 
 function getUpcomingBirthdays() {
-	const dateRange = [new Date(), new Date()]
-	dateRange[1].setMonth(dateRange[0].getMonth() + 1);
-
+	const n = 5
 	return inhabitantsStore.inhabitants
 		.map(inhabitant => ({
 			...inhabitant,
 			nextBirthday: getNextBirthday(inhabitant.date_of_birth)
 		}))
-		.filter(inhabitant => inhabitant.nextBirthday >= dateRange[0] && inhabitant.nextBirthday <= dateRange[1])
-		.sort((a, b) => a.nextBirthday - b.nextBirthday); // Sort by the closest birthday
+		.filter(inhabitant => inhabitant.nextBirthday >= new Date())
+		.sort((a, b) => a.nextBirthday - b.nextBirthday) // Sort by the closest birthday
+		.slice(0, n)
 }
 </script>
